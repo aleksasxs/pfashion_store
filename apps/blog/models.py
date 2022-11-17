95ea59e75174de4a99892c9b868d712a39c19e2b
aleksasxs <117458923+aleksasxs@users.noreply.github.com>
2022-11-17 19:14:36 +0100

    Tags

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -25,3 +25,15 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name = 'Тэг', max_length = 255)
+    tags = models.ManyToManyField(Article)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тэг статьи'
+        verbose_name_plural = 'Тэги статьи'
