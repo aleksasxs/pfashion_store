a03810034dfd544fbc6368fb34fe8b72e63b356d
aleksasxs <117458923+aleksasxs@users.noreply.github.com>
2022-12-12 18:42:49 +0100

    Tags search

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,6 +4,7 @@ from apps.blog.views import blog_category_list, article_list, article_view, tag_
 urlpatterns = [
     path('', blog_category_list, name='blog_category_list'),
     path('<int:category_id>/', article_list, name='blog_article_list'),
-    path('<int:category_id>/<int:article_id>/', article_view, name='blog_article_view'),
-    path('<int:tag_id>/',tag_view, name='tag_view')
+    path('tag/<int:tag_id>/',tag_view, name='tag_view'),
+    path('<int:category_id>/<int:article_id>/', article_view, name='blog_article_view')
+
 ]
