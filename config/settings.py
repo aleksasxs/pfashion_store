7f7f8f29145d667010cc3237515db01093dc3c4d
aleksasxs <117458923+aleksasxs@users.noreply.github.com>
2022-12-23 17:13:04 +0100

    User app

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'apps.blog'
+    'phonenumber_field',
+
+
+    'apps.blog',
+    'apps.user',
+
 ]
 
 MIDDLEWARE = [
@@ -133,3 +138,4 @@ MEDIA_ROOT = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.User'
