d60b5bb00eb38f2dcc1c6c3e8d4f6d296caadc5b
aleksasxs <117458923+aleksasxs@users.noreply.github.com>
2023-01-27 13:34:06 +0100

    Homework

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
 from apps.user.forms import LoginForm, RegisterForm
 
 
@@ -14,7 +16,8 @@ def user_login(request):
                 login(request, user)
                 return redirect('index')
         error = 'Неправильные логин или пароль'
-    return render(request, 'user/login.html', {'error': error})
+    breadcrumbs = {'current': 'Войти'}
+    return render(request, 'user/login.html', {'error': error, 'breadcrumbs': breadcrumbs})
 
 def user_register(request):
     error = None
@@ -24,9 +27,13 @@ def user_register(request):
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            return render(request, 'user/welcome.html', {'user': user})
+            breadcrumbs = {
+                reverse('register'): 'Регистрация',
+                'current': 'Здравствуйте!'}
+            return render(request, 'user/welcome.html', {'user': user, 'breadcrumbs': breadcrumbs})
         error = form.errors
-    return render(request, "user/register.html", {'error': error})
+    breadcrumbs = {'current': 'Здравствуйте!'}
+    return render(request, "user/register.html", {'error': error, 'breadcrumbs': breadcrumbs})
 
 def user_logout(request):
     logout(request)
