77a19429a6f3531358a59cf99d27af9840f8c3e2
aleksasxs <117458923+aleksasxs@users.noreply.github.com>
2023-02-14 15:59:34 +0100

    Drf categories

--- apps/api/catalog/views.py
+++ apps/api/catalog/views.py
@@ -26,26 +26,14 @@ class ProductDeleteView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     permission_classes = [permissions.IsAdminUser]
 
-class CategoryListView(generics.ListAPIView):
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            return [permission() for permission in [permissions.IsAdminUser]]
+        return [permission() for permission in [permissions.AllowAny]]
 
-class CategoryDetailView(generics.RetrieveAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-
-class CategoryCreateView(generics.CreateAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    permission_classes = [permissions.IsAdminUser]
-
-class CategoryUpdateView(generics.UpdateAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    permission_classes = [permissions.IsAdminUser]
-
-class CategoryDeleteView(generics.DestroyAPIView):
-    serializer_class = CategorySerializer
-    queryset = Category.objects.all()
-    permission_classes = [permissions.IsAdminUser]
