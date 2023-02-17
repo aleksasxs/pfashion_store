52b1cf695db2c71308715cae2219c4495c536825
aleksasxs <117458923+aleksasxs@users.noreply.github.com>
2023-02-17 17:10:06 +0100

    Drf part 4

--- apps/api/blog/urls.py
+++ apps/api/blog/urls.py
@@ -1,12 +1,9 @@
-from django.urls import path
-from apps.api.blog.views import ArticleListView, ArticleCreateView, ArticleDeleteView, ArticleDetailView,\
-    ArticleUpdateView
+from apps.api.blog.views import ArticleViewSet
 from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('article/', ArticleListView.as_view()),
-    path('article/<int:pk>/', ArticleDetailView.as_view()),
-    path('article/create/', ArticleCreateView.as_view()),
-    path('article/update/<int:pk>/', ArticleUpdateView.as_view()),
-    path('article/delete/<int:pk>/', ArticleDeleteView.as_view()),
-]
+urlpatterns = []
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
+
+urlpatterns += router.urls
